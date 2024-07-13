66c7b17ad6ed11c1ac921df39cad1da18b81f976
DutchakKristina <dutchakk7@gmail.com>
2024-07-12 22:49:03 -0600

    base url changed

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const BASE_URL = 'https://6692004826c2a69f6e913a1f.mockapi.io/Contactsç';
+const BASE_URL = 'https://6692004826c2a69f6e913a1f.mockapi.io/Contacts';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
